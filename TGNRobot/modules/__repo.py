026d8c8412c8554a2633be93ce9375f4a218cdc6
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-11-09 22:55:29 +0530

    Update __repo.py

--- TGNRobot/modules/__repo.py
+++ TGNRobot/modules/__repo.py
@@ -5,15 +5,13 @@ from pyrogram.types import *
 from TGNRobot.conf import get_str_key
 from TGNRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/3f3c1824acae809163899.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [𝐑𝐀𝐉](t.me/JaiHindChatting) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @TrishaNetwork «««"
+REPO_TEXT = "**A Powerful [BOT](t.me/DevilTrishaRoBot) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [𝐑𝐀𝐉](t.me/JaiHindChatting) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» 😈🇮🇳😈🇮🇳😈 «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
         InlineKeyboardButton("⚡ ʀᴇᴘᴏꜱɪᴛᴏʀʏ🔥", url=f"https://github.com/rakeshyt/TrishaManager"),
       ],[
         InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ ⚡", url="https://t.me/JaiHindChatting"),
-      ],[
-        InlineKeyboardButton("⚡ ᴜᴘᴅᴀᴛᴇꜱ ☑️", url="https://t.me/TrishaNetwork"),
       ]]
     )
   
