60bd3955311dd287a40c2029e2cb863068f61e84
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-11-22 23:03:35 +0530

    Update sample_config.py

--- TGNRobot/sample_config.py
+++ TGNRobot/sample_config.py
@@ -17,9 +17,9 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "boom"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1669178360  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 2023126723  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Timesisnotwaiting" 
-    SUPPORT_CHAT = "GodfatherSupport"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "JaiHindChatting"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001392103597
     )  # Prints any new group the bot is added to, prints just the name and ID.
